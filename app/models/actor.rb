d4c96a79f0cd578546895b7a64d9ab7728a88caf
Ece Ozalp <github email address>
2016-08-09 19:43:31 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,65 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name.capitalize} #{self.last_name.capitalize}"
+  end
+
+  def list_roles
+    self.characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
+
+
+
+# class Choice
+#   belongs_to :user
+#   belongs_to :answer
+#   has_one :question, :through => :answer
+# end
+
+
+# class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
+#   def change
+#     add_column :students, :grade, :integer
+#     add_column :students, :birthdate, :string
+#   end
+# end
+
+
+# def self.highest_rating
+#   self.maximum(:rating)
+# end
+
+# def self.most_popular_show
+#   self.find_by(rating: self.maximum(:rating))
+# end
+
+# def self.lowest_rating
+#   self.minimum(:rating)
+# end
+
+# def self.least_popular_show
+#   self.find_by(rating: (self.minimum(:rating)))
+# end
+
+# def self.ratings_sum
+#   self.sum(:rating)
+# end
+
+# def self.shows_by_alphabetical_order
+#   self.order(:name)
+# end
+
+# def self.popular_shows
+#   self.where("rating > 5")
+# end
+
+
+# has_many :tweets
+
+#   validates :username, :uniqueness => true
+# #(callback methods)  after_save :show_error_message
---
+++ db/migrate/009_add_genre_to_shows_table.rb
@@ -0,0 +1,5 @@
+class AddGenreToShowsTable < ActiveRecord::Migration
+  def change
+    add_column :shows, :genre, :string
+  end
+end
